268d4fcbd11d9c23ba139b194bb1c8901c636457
jalfvort <jon.alfaro@direktiv.io>
2022-09-01 10:19:32 +1000

    Component content-panel types defined
    Signed-off-by: jalfvort <jon.alfaro@direktiv.io>

---
+++ src/components/content-panel/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import FlexBox from '../flexbox';
+import './style.css';
+
+export interface ContentPanelProps extends React.HTMLAttributes<HTMLDivElement> {
+    grow: boolean
+}
+
+const ContentPanel: React.FunctionComponent<ContentPanelProps> = ({
+    grow = false,
+    ...props
+}) => {
+    return (
+        <div {...props} className={`content-panel-parent opaque ${props.className ? props.className : ""}`} style={{ display: "flex", flexDirection: "column", flexGrow: grow ? "1" : undefined}} />
+    );
+}
+
+export default ContentPanel;
+
+export function ContentPanelTitle({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
+    return (
+        <FlexBox {...props} className={`content-panel-title ${props.className ? props.className : ""}`} />
+    );
+}
+
+export function ContentPanelTitleIcon({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
+    return (
+        <div {...props} className={`content-panel-title-icon ${props.className ? props.className : ""}`} />
+    );
+}
+
+export function ContentPanelBody({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
+    return (
+        <div {...props} className={`content-panel-body ${props.className ? props.className : ""}`} />
+    );
+}
+
+export function ContentPanelFooter({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
+    return (
+        <div {...props} className={`content-panel-footer ${props.className ? props.className : ""}`} />
+    );
+}
