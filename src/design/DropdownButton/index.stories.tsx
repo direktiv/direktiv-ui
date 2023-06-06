e31b559df74fa9b3072872bb91a452bf8dc8b465
porleaaron <porleaaron28@gmail.com>
2023-06-07 00:17:58 +0800

    prettier fix

--- src/design/DropdownButton/index.stories.tsx
+++ src/design/DropdownButton/index.stories.tsx
@@ -4,149 +4,149 @@ import { GitBranchIcon, Users } from "lucide-react";
 import { DropdownMenuGroup, DropdownMenuItem } from "../Dropdown";
 
 const meta = {
-    title: "Components/DropdownButton",
-    component: DropdownButton,
+  title: "Components/DropdownButton",
+  component: DropdownButton,
 } satisfies Meta<typeof DropdownButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    render: () => <DropdownButtonRoot>
-        <PrimaryButton >
-            Review
-            <GitBranchIcon />
-        </PrimaryButton>
-        <DropdownButton >
-            <DropdownMenuGroup>
-                <DropdownMenuItem>
-                    <Users className="mr-2 h-4 w-4" />
-                    <span>Team</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <span>New Team</span>
-                </DropdownMenuItem>
-            </DropdownMenuGroup>
-        </DropdownButton>
-    </DropdownButtonRoot>,
-    tags: ["autodocs"],
+  render: () => (
+    <DropdownButtonRoot>
+      <PrimaryButton>
+        Review
+        <GitBranchIcon />
+      </PrimaryButton>
+      <DropdownButton>
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Users className="mr-2 h-4 w-4" />
+            <span>Team</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <span>New Team</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownButton>
+    </DropdownButtonRoot>
+  ),
+  tags: ["autodocs"],
 };
 
 export const Variants = () => (
-    <DropdownButtonRoot>
-        <PrimaryButton variant="destructive">
-            Review
-            <GitBranchIcon />
-        </PrimaryButton>
-        <DropdownButton variant="primary">
-            <DropdownMenuGroup>
-                <DropdownMenuItem>
-                    <Users className="mr-2 h-4 w-4" />
-                    <span>Team</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <span>New Team</span>
-                </DropdownMenuItem>
-            </DropdownMenuGroup>
-        </DropdownButton>
-    </DropdownButtonRoot>
-)
+  <DropdownButtonRoot>
+    <PrimaryButton variant="destructive">
+      Review
+      <GitBranchIcon />
+    </PrimaryButton>
+    <DropdownButton variant="primary">
+      <DropdownMenuGroup>
+        <DropdownMenuItem>
+          <Users className="mr-2 h-4 w-4" />
+          <span>Team</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <span>New Team</span>
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+    </DropdownButton>
+  </DropdownButtonRoot>
+);
 export const Disabled = () => (
-    <DropdownButtonRoot disabled>
-        <PrimaryButton variant="destructive">
-            Review
-            <GitBranchIcon />
-        </PrimaryButton>
-        <DropdownButton variant="primary">
-            <DropdownMenuGroup>
-                <DropdownMenuItem>
-                    <Users className="mr-2 h-4 w-4" />
-                    <span>Team</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <span>New Team</span>
-                </DropdownMenuItem>
-            </DropdownMenuGroup>
-        </DropdownButton>
-    </DropdownButtonRoot>
-)
+  <DropdownButtonRoot disabled>
+    <PrimaryButton variant="destructive">
+      Review
+      <GitBranchIcon />
+    </PrimaryButton>
+    <DropdownButton variant="primary">
+      <DropdownMenuGroup>
+        <DropdownMenuItem>
+          <Users className="mr-2 h-4 w-4" />
+          <span>Team</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <span>New Team</span>
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+    </DropdownButton>
+  </DropdownButtonRoot>
+);
 export const ButtonSize = () => (
-    <div>
-
-        <DropdownButtonRoot>
-            <PrimaryButton variant="destructive" size="lg">
-                LG Button
-                <GitBranchIcon />
-            </PrimaryButton>
-            <DropdownButton variant="primary" size="lg">
-                <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                        <Users className="mr-2 h-4 w-4" />
-                        <span>Team</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <span>New Team</span>
-                    </DropdownMenuItem>
-                </DropdownMenuGroup>
-            </DropdownButton>
-        </DropdownButtonRoot>
-        <DropdownButtonRoot>
-            <PrimaryButton variant="destructive" size="sm">
-                SM Button
-                <GitBranchIcon />
-            </PrimaryButton>
-            <DropdownButton variant="primary" size="sm">
-                <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                        <Users className="mr-2 h-4 w-4" />
-                        <span>Team</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <span>New Team</span>
-                    </DropdownMenuItem>
-                </DropdownMenuGroup>
-            </DropdownButton>
-        </DropdownButtonRoot>
-    </div>
-
-)
+  <div>
+    <DropdownButtonRoot>
+      <PrimaryButton variant="destructive" size="lg">
+        LG Button
+        <GitBranchIcon />
+      </PrimaryButton>
+      <DropdownButton variant="primary" size="lg">
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Users className="mr-2 h-4 w-4" />
+            <span>Team</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <span>New Team</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownButton>
+    </DropdownButtonRoot>
+    <DropdownButtonRoot>
+      <PrimaryButton variant="destructive" size="sm">
+        SM Button
+        <GitBranchIcon />
+      </PrimaryButton>
+      <DropdownButton variant="primary" size="sm">
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Users className="mr-2 h-4 w-4" />
+            <span>Team</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <span>New Team</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownButton>
+    </DropdownButtonRoot>
+  </div>
+);
 
 export const ButtonBlock = () => (
-    <DropdownButtonRoot block>
-        <PrimaryButton variant="destructive">
-            Review
-            <GitBranchIcon />
-        </PrimaryButton>
-        <DropdownButton variant="primary">
-            <DropdownMenuGroup>
-                <DropdownMenuItem>
-                    <Users className="mr-2 h-4 w-4" />
-                    <span>Team</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <span>New Team</span>
-                </DropdownMenuItem>
-            </DropdownMenuGroup>
-        </DropdownButton>
-    </DropdownButtonRoot>
-)
+  <DropdownButtonRoot block>
+    <PrimaryButton variant="destructive">
+      Review
+      <GitBranchIcon />
+    </PrimaryButton>
+    <DropdownButton variant="primary">
+      <DropdownMenuGroup>
+        <DropdownMenuItem>
+          <Users className="mr-2 h-4 w-4" />
+          <span>Team</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <span>New Team</span>
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+    </DropdownButton>
+  </DropdownButtonRoot>
+);
 
 export const ButtonLoading = () => (
-    <DropdownButtonRoot block disabled>
-        <PrimaryButton variant="destructive" loading>
-            Review
-            <GitBranchIcon />
-        </PrimaryButton>
-        <DropdownButton variant="primary">
-            <DropdownMenuGroup>
-                <DropdownMenuItem>
-                    <Users className="mr-2 h-4 w-4" />
-                    <span>Team</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <span>New Team</span>
-                </DropdownMenuItem>
-            </DropdownMenuGroup>
-        </DropdownButton>
-    </DropdownButtonRoot>
-)
+  <DropdownButtonRoot block disabled>
+    <PrimaryButton variant="destructive" loading>
+      Review
+      <GitBranchIcon />
+    </PrimaryButton>
+    <DropdownButton variant="primary">
+      <DropdownMenuGroup>
+        <DropdownMenuItem>
+          <Users className="mr-2 h-4 w-4" />
+          <span>Team</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <span>New Team</span>
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+    </DropdownButton>
+  </DropdownButtonRoot>
+);
--- src/design/DropdownButton/index.tsx
+++ src/design/DropdownButton/index.tsx
@@ -1,84 +1,91 @@
 import Button, { ButtonProps } from "../Button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "../Dropdown";
-import React, { ButtonHTMLAttributes, HTMLAttributes } from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "../Dropdown";
+import React, { ButtonHTMLAttributes, HTMLAttributes } from "react";
 
 import { ChevronDown } from "lucide-react";
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 interface RootProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    block?: boolean
+  block?: boolean;
 }
-export const DropdownButtonRoot = React.forwardRef<HTMLButtonElement, RootProps>(
-    ({ block, children, ...props }, ref) => (
-        <button
-            ref={ref}
-            {...props}
-            className={clsx(
-                block ? "w-full" : "w-fit",
-                "flex-row flex",
-                "disabled:pointer-events-none disabled:opacity-50",
-            )}
-        >
-            <DropdownMenu>
-                {children}
-            </DropdownMenu>
-        </button>
-    )
-);
+export const DropdownButtonRoot = React.forwardRef<
+  HTMLButtonElement,
+  RootProps
+>(({ block, children, ...props }, ref) => (
+  <button
+    ref={ref}
+    {...props}
+    className={clsx(
+      block ? "w-full" : "w-fit",
+      "flex flex-row",
+      "disabled:pointer-events-none disabled:opacity-50"
+    )}
+  >
+    <DropdownMenu>{children}</DropdownMenu>
+  </button>
+));
 DropdownButtonRoot.displayName = "DropdownButtonRoot";
 
 const ButtonFragmentClass = clsx(
-    "active:scale-100 active:outline-none active:ring-0 active:ring-offset-0",
-    "border-2 focus:ring-0 focus:ring-offset-0",
-)
+  "active:scale-100 active:outline-none active:ring-0 active:ring-offset-0",
+  "border-2 focus:ring-0 focus:ring-offset-0"
+);
 
 const ButtonColorClass = (variant: string | undefined) => {
-    if (!variant) {
-        return "dark:bg-gray-dark-1 dark:hover:bg-gray-dark-3 dark:active:bg-gray-dark-4  dark:text-gray-dark-11 dark:border-gray-dark-10 bg-gray-1 hover:bg-gray-3 active:bg-gray-4 text-gray-11 border-gray-10"
-    } else {
-        return ""
-    }
-}
+  if (!variant) {
+    return "dark:bg-gray-dark-1 dark:hover:bg-gray-dark-3 dark:active:bg-gray-dark-4  dark:text-gray-dark-11 dark:border-gray-dark-10 bg-gray-1 hover:bg-gray-3 active:bg-gray-4 text-gray-11 border-gray-10";
+  } else {
+    return "";
+  }
+};
 
-export const PrimaryButton = React.forwardRef<HTMLButtonElement, HTMLAttributes<HTMLButtonElement> & ButtonProps>(
-    ({ variant, children, ...props }, ref) => {
-        return (
-            <Button
-                variant={variant}
-                ref={ref} {...props}
-                className={clsx(
-                    "flex flex-row rounded-none rounded-l-md flex-1",
-                    ButtonColorClass(variant),
-                    ButtonFragmentClass,
-                )
-                }
-            >
-                {children}
-            </Button>
-        )
-    }
-);
-PrimaryButton.displayName = "PrimaryButton"
+export const PrimaryButton = React.forwardRef<
+  HTMLButtonElement,
+  HTMLAttributes<HTMLButtonElement> & ButtonProps
+>(({ variant, children, ...props }, ref) => {
+  return (
+    <Button
+      variant={variant}
+      ref={ref}
+      {...props}
+      className={clsx(
+        "flex flex-1 flex-row rounded-none rounded-l-md",
+        ButtonColorClass(variant),
+        ButtonFragmentClass
+      )}
+    >
+      {children}
+    </Button>
+  );
+});
+PrimaryButton.displayName = "PrimaryButton";
 
-export const DropdownButton = React.forwardRef<HTMLButtonElement, ButtonHTMLAttributes<HTMLButtonElement> & ButtonProps>(
-    ({ children, variant, ...props }, ref) => (
-        <div className="flex ">
-            <DropdownMenuTrigger asChild ref={ref} {...props}>
-                <Button
-                    variant={variant}
-                    icon className={twMerge(
-                        ButtonFragmentClass,
-                        ButtonColorClass(variant),
-                        "rounded-none rounded-r-md border-l-0"
-                    )}>
-                    <ChevronDown />
-                </Button>
-            </DropdownMenuTrigger>
-            {children && <DropdownMenuContent className="w-56">
-                {children}
-            </DropdownMenuContent>}
-        </div>
-    )
-)
+export const DropdownButton = React.forwardRef<
+  HTMLButtonElement,
+  ButtonHTMLAttributes<HTMLButtonElement> & ButtonProps
+>(({ children, variant, ...props }, ref) => (
+  <div className="flex ">
+    <DropdownMenuTrigger asChild ref={ref} {...props}>
+      <Button
+        variant={variant}
+        icon
+        className={twMerge(
+          ButtonFragmentClass,
+          ButtonColorClass(variant),
+          "rounded-none rounded-r-md border-l-0"
+        )}
+      >
+        <ChevronDown />
+      </Button>
+    </DropdownMenuTrigger>
+    {children && (
+      <DropdownMenuContent className="w-56">{children}</DropdownMenuContent>
+    )}
+  </div>
+));
 DropdownButton.displayName = "DropdownButton";
