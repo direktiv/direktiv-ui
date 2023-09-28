85a21808418e9f9be7c44e5b28f146179dd7e13d
porleaaron <porleaaron28@gmail.com>
2023-09-28 14:29:29 +0800

    remove hard coded origin

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -5,7 +5,7 @@ const authFile = "playwright/.auth/user.json";
 setup("authenticate", async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
 
-  await page.goto("http://localhost:3333/");
+  await page.goto("/");
   await page.getByTestId("api-key-input").fill("Qhxw6U3#6&hu^j");
   await page.getByTestId("api-key-set-btn").click();
   // Wait until the page receives the cookies.
