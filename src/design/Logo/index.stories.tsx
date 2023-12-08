b682a7e7c0b1cb776329cdf6c4be95151b747957
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-08 09:22:00 +0100

    add CustomLogo component
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Logo/index.stories.tsx
+++ src/design/Logo/index.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Logo from "./index";
+import { Switch } from "../Switch";
+import { useState } from "react";
 
 const meta = {
   title: "Components/Logo",
@@ -39,3 +41,37 @@ export const PassClassNames = () => (
     <Logo customLogo={false} />
   </div>
 );
+
+export const CustomLogo = () => {
+  const [darkMode, setDarkmode] = useState(false);
+  return (
+    <div className="flex flex-col gap-5">
+      <div>
+        providing a custom logo to the component, requires to set a path for
+        both light and dark mode and explicitly set the useDarkMode prop to
+        either true or false.
+      </div>
+      <div>
+        The custom logo
+        <label htmlFor="dark-mode" style={{ paddingRight: 15 }}>
+          Dark Mode
+        </label>
+        <Switch
+          id="dark-mode"
+          value={darkMode ? "on" : "off"}
+          onCheckedChange={(newVal) => {
+            setDarkmode(newVal);
+          }}
+        />
+      </div>
+      <div>
+        <Logo
+          customLogo={true}
+          pathLightMode="https://placehold.jp/f5f5f5/000000/160x30.png"
+          pathDarkMode="https://placehold.jp/000000/ffffff/160x30.png"
+          useDarkMode={darkMode}
+        />
+      </div>
+    </div>
+  );
+};
